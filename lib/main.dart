62ea06322e6b9b47c946674c2d39fa98644f06d4
HarshSahu23 <harshsahu7765858311@gmail.com>
2023-09-28 22:02:15 +0530

    Changes to be committed: 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:demo_sih7/LandingPage/landingPage.dart';
 import 'package:flutter/material.dart';
 import 'Screens/theme.dart';
 import 'dart:async';
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:camera/camera.dart';
 
